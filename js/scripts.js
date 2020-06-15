3473e2673547c7e672aa1995be83583e0d60602e
Joseph Pearce <joseph.pearce05@gmail.com>
2020-06-15 10:14:11 -0700

    add address book project

---
+++ js/scripts.js
@@ -0,0 +1,52 @@
+// Business Logic
+
+function AddressBook() {
+  this.contacts = [];
+  this.currentID = 0;
+}
+
+AddressBook.prototype.addContact = function(contact) {
+  contact.id = this.assignID(); 
+  this.contacts.push(contact);
+}
+
+AddressBook.prototype.assignID = function() {
+  this.currentID += 1;
+  return this.currentID;
+}
+
+AddressBook.prototype.findContact = function (id) {
+  for (let i=0; i< this.contacts.length; i++) {
+    if (this.contacts[i]) {
+      if (this.contacts[i].id == id) {
+        return this.contacts[i];
+      }
+    }
+  };
+  return false;
+}
+
+AddressBook.prototype.deleteContact = function(id) {
+  for (let i=0; i< this.contacts.length; i++) {
+    if (this.contacts[i]) {
+      if (this.contacts[i].id == id) {
+        delete this.contacts[i];
+        return true;
+      }
+    }
+  };
+  return false;
+}
+
+// Business Logic for Contacts
+
+function Contact(firstName, lastName, phoneNumber) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.phoneNumber = phoneNumber;
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + "" + this.lastName;
+}
+
